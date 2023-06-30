8db2d3cd49a9e7803b7a2b7150f1e08712648971
labinochka <labensckayam@yandex.ru>
2023-06-30 13:41:25 +0300

    update editText

--- app/src/main/java/com/itis/summerpractice/MainActivity.kt
+++ app/src/main/java/com/itis/summerpractice/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.core.widget.addTextChangedListener
 
 class MainActivity : AppCompatActivity() {
     private var clickButton: Button? = null
@@ -17,18 +18,41 @@ class MainActivity : AppCompatActivity() {
         val textView = findViewById<TextView>(R.id.textView2)
         clickButton = findViewById<Button>(R.id.button)
 
-        clickButton?.setOnClickListener {
-            val name = findViewById<EditText>(R.id.editTextNumber3).text
-            val age = findViewById<EditText>(R.id.editTextNumber2).text.toString().toInt()
-            val height = findViewById<EditText>(R.id.editTextNumber).text.toString().toInt()
-            val weight = findViewById<EditText>(R.id.editTextNumberDecimal).text.toString().toDouble()
-            if (name.length in 1..50 && age in 1..149 &&
-                height in 1..249 && weight in 1.0..249.0) {
-                answer = (name.length + age + height + weight + 1000).toInt()
-                textView.text = "Answer: $answer"
-            } else {
-                textView.text = "Incorrect data"
+        val nameEdit = findViewById<EditText>(R.id.editTextNumber3)
+        nameEdit.addTextChangedListener {
+            if (nameEdit.text.length !in 1..50) {
+                nameEdit.error = "Incorrect data"
             }
+        }
+
+        val ageEdit = findViewById<EditText>(R.id.editTextNumber2)
+        ageEdit.addTextChangedListener {
+            if ((ageEdit.text.toString() == "") || (ageEdit.text.toString().toInt() !in 1..149)) {
+                ageEdit.error = "Incorrect data"
+            }
+        }
+
+        val heightEdit = findViewById<EditText>(R.id.editTextNumber)
+        heightEdit.addTextChangedListener {
+            if ((heightEdit.text.toString() == "") || (heightEdit.text.toString().toInt() !in 1..249)) {
+                heightEdit.error = "Incorrect data"
+            }
+        }
+
+        val weightEdit = findViewById<EditText>(R.id.editTextNumberDecimal)
+        weightEdit.addTextChangedListener {
+            if ((weightEdit.text.toString() == "") ||(weightEdit.text.toString().toDouble() !in 1.0..249.0)) {
+                weightEdit.error = "Incorrect data"
+            }
+        }
+
+        clickButton?.setOnClickListener {
+            val name = nameEdit.text
+            val age = ageEdit.text.toString().toInt()
+            val height = heightEdit.text.toString().toInt()
+            val weight = weightEdit.text.toString().toDouble()
+            answer = (name.length + age + height + weight + 1000).toInt()
+            textView.text = "Answer: $answer"
         }
     }
 }
